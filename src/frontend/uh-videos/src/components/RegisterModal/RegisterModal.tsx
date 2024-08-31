de5611ac2b61bb428872d169e1c1f18c5e21c3b3
chriss8g <chriss.8.g@citmatel.inf.cu>
2024-08-31 17:16:43 -0400

    fix migrate to tsx

--- src/frontend/uh-videos/src/components/RegisterModal/RegisterModal.tsx
+++ src/frontend/uh-videos/src/components/RegisterModal/RegisterModal.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 // Modal
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function LoginModal() {
+function RegisterModal() {
     // Modal
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
@@ -12,31 +12,36 @@ function LoginModal() {
     const handleSubmit = () => {
         console.log('enviando los datos')
     }
-    
+
     return (
         <>
-            <Button variant="success" onClick={handleShow}>
-            Login
+            <Button variant="primary" onClick={handleShow}>
+                Register
             </Button>
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Welcome again</Modal.Title>
+                    <Modal.Title>Who are you?</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <form onSubmit={handleSubmit}>
                         <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Name</label>
+                            <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Jonhson" />
+                        </div>
+                        <div className="mb-3">
                             <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                         </div>
                         <div className='d-flex justify-content-center'>
                             <button className="btn btn-primary" type="submit">Submit form</button>
                         </div>
                     </form>
                 </Modal.Body>
+
             </Modal>
         </>
     )
 }
 
-export default LoginModal;
+export default RegisterModal;
